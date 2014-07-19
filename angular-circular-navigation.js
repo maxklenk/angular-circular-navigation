5b696c54bb19703623f5a279c38a104022610925
Max Klenk <max@klenk.biz>
2014-07-19 20:59:51 +0200

    generate items based on options

--- angular-circular-navigation.js
+++ angular-circular-navigation.js
@@ -11,12 +11,22 @@ angular.module('angularCircularNavigation', [])
           options: '='
         },
         // templateUrl: 'angular-circular-navigation.html',
-        template: '<button ng-click="toggleMenu()" class="cn-button" id="cn-button">Menu</button><div class="cn-wrapper" ng-class="{\'opened-nav\': options.isOpen}" id="cn-wrapper"><ul><li><a href="#"><span>About</span></a></li><li><a href="#"><span>Tutorials</span></a></li><li><a href="#"><span>Articles</span></a></li><li><a href="#"><span>Snippets</span></a></li><li><a href="#"><span>Plugins</span></a></li><li><a href="#"><span>Contact</span></a></li><li><a href="#"><span>Follow</span></a></li></ul></div>',
+        template: '<button ng-click="toggleMenu()" class="cn-button">{{options.content}}</button><div class="cn-wrapper" ng-class="{\'opened-nav\': options.isOpen}"><ul><li ng-repeat="item in options.items"><a ng-click="perform(item)" class="{{item.cssClass}}"><span>{{item.content}}</span></a></li></ul></div>',
         controller: ['$scope', '$element', '$attrs',
           function ($scope, $element, $attrs) {
 
             $scope.toggleMenu = function () {
               $scope.options.isOpen = !$scope.options.isOpen;
+            };
+
+            $scope.perform = function (item) {
+              if (typeof item.onclick === 'function') {
+                item.onclick();
+              }
+
+              if ($scope.options.toggleOnClick) {
+                $scope.toggleMenu();
+              }
             };
 
           }
