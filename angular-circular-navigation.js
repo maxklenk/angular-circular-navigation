477b02d336689d308142d283ae81b71d0c8b07ab
Max Klenk <max@klenk.biz>
2014-07-20 12:27:05 +0200

    add isActive and background in scss

--- angular-circular-navigation.js
+++ angular-circular-navigation.js
@@ -11,7 +11,12 @@ angular.module('angularCircularNavigation', [])
           options: '='
         },
         // templateUrl: 'angular-circular-navigation.html',
-        template: '<button ng-click="toggleMenu()" class="cn-button" ng-class="options.cssClass">{{options.content}}</button><div class="cn-wrapper items-{{options.items.length}}" ng-class="{\'opened-nav\': options.isOpen}"><ul><li ng-repeat="item in options.items"><a ng-click="perform(item)" class="{{item.cssClass}}"><span>{{item.content}}</span></a></li></ul></div>',
+        template: '<button ng-click="toggleMenu()" class="cn-button" ng-class="options.cssClass">{{options.content}}</button>' + 
+                  '<div class="cn-wrapper items-{{options.items.length}}" ng-class="{\'opened-nav\': options.isOpen}"><ul>' +
+                    '<li ng-repeat="item in options.items">' +
+                      '<a ng-click="perform(item)" ng-class="{\'is-active\': item.isActive}" class="{{item.cssClass}}">' +
+                        '<span>{{item.content}}</span>' +
+                      '</a></li></ul></div>',
         controller: ['$scope', '$element', '$attrs',
           function ($scope, $element, $attrs) {
 
@@ -23,6 +28,8 @@ angular.module('angularCircularNavigation', [])
               if (typeof item.onclick === 'function') {
                 item.onclick();
               }
+
+              item.isActive = !item.isActive;
 
               if ($scope.options.toggleOnClick) {
                 $scope.toggleMenu();
